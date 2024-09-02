1f3b29537efb980957905083369ccf3d4b9f3365
checkr <ugochiori@gmail.com>
2024-09-02 10:29:25 +0100

    icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { BellIcon } from '@heroicons/react/16/solid';
-import InvoicePage from './invoicemodal/InvoicePage';
+import React, { useState } from "react";
+// import { BellIcon } from '@heroicons/react/16/solid';
+import InvoicePage from "./invoicemodal/InvoicePage";
+import { ChevronDownIcon } from "@heroicons/react/16/solid";
 
 const Header: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -18,22 +19,35 @@ const Header: React.FC = () => {
       <header className="mb-8">
         {/* Top Title Section */}
         <div className="flex justify-between items-center mb-4">
-          <h1 className="text-[20px] md:text-[26px] font-semibold tracking-wide text-gray-600">INVOICE</h1>
-          <div className="flex items-center">
-            <BellIcon className="w-5 h-5 md:w-6 md:h-6" />
-            <div className="flex items-center justify-center w-8 h-8 md:w-10 md:h-10 bg-gray-200 rounded-full">
-              <span className="text-customBlue font-semibold text-[14px] md:text-base">KD</span>
+          <h1 className="text-[20px] md:text-[26px] text-[#373B47] font-semibold tracking-wide ">
+            INVOICE
+          </h1>
+          <div className="flex items-center gap-[16px]">
+            <div className="flex flex-col  justify-center items-center w-[64px] h-[64px] rounded-full bg-[#FFFFFF]">
+            <img
+              src="/images/bell.svg "
+              alt="bell"
+              className="w-6 h-6 text-gray-400 cursor-pointer "
+            />
+</div>
+            <div className="flex flex-row items-center justify-between w-[88px] h-[64px] bg-[#FFFFFF] rounded-full">
+              <span className="flex items-center justify-center border border-customBlue rounded-full text-[#ffff] bg-customBlue text-center font-semibold md:text-[16px] md:text-base uppercase w-[40px] h-[40px] ml-2">
+                KO
+              </span>
+              <ChevronDownIcon className="w-6 h-6 text-gray-400 mr-1 cursor-pointer" />
             </div>
+
+           
           </div>
         </div>
 
-        <div className="border-b border-gray-300 mb-4"></div>
+        <div className="border-b border-gray-200 mb-4"></div>
 
         {/* Secondary Title and Buttons */}
         <div className="flex flex-col items-center md:flex-row md:justify-between md:items-center mb-2">
-          <h2 className="text-[18px] md:text-[24px] font-normal">Invoice</h2>
+          <h2 className="text-[18px] md:text-[30px] text-[#1F1F23] font-[500]">Invoice</h2>
           <div className="flex flex-col items-center md:flex-row">
-            <button className="w-[75vw] md:w-[240px] h-[45px] md:h-[60px] text-gray-500 border rounded-full px-2 md:px-4 py-1 md:py-2 mr-2 md:mr-4 bg-white text-[12px] md:text-base">
+            <button className="w-[75vw] md:w-[240px] h-[45px] md:h-[60px] text-[#697598] border rounded-full px-2 md:px-4 py-1 md:py-2 mr-2 md:mr-4 bg-white text-[14px] md:text-base">
               SEE WHAT'S NEW
             </button>
             <button
@@ -45,7 +59,9 @@ const Header: React.FC = () => {
           </div>
         </div>
       </header>
-      {isModalOpen && <InvoicePage isOpen={isModalOpen} onClose={handleCloseModal} />}
+      {isModalOpen && (
+        <InvoicePage isOpen={isModalOpen} onClose={handleCloseModal} />
+      )}
     </>
   );
 };
--- src/components/InvoiceStats.tsx
+++ src/components/InvoiceStats.tsx
@@ -3,10 +3,10 @@ import { HiOutlineViewGrid } from 'react-icons/hi';
 
 const InvoiceStats: React.FC = () => {
   const stats = [
-    { title: 'Total Paid', value: '$4,120,102.', color: 'bg-green-500', decimal: '76' },
-    { title: 'Total Overdue', value: '$23,000.', color: 'red-500', decimal: '13' },
-    { title: 'Total Draft', value: '$12,200.', color: 'gray-400', decimal: '00'},
-    { title: 'Total Unpaid', value: '$87,102.', color: 'text-yellow-500', decimal: '00' },
+    { title: 'Total Paid', value: '$4,120,102.', color: '1,289', decimal: '76' },
+    { title: 'Total Overdue', value: '$23,000.', color: '13', decimal: '13' },
+    { title: 'Total Draft', value: '$12,200.', color: '08', decimal: '00'},
+    { title: 'Total Unpaid', value: '$87,102.', color: '06', decimal: '00' },
   ];
 
   return (
@@ -18,8 +18,8 @@ const InvoiceStats: React.FC = () => {
           className={`p-4 bg-white rounded-lg border  h-48  flex flex-col justify-center items-start gap-4 uppercase `}
         >
           <HiOutlineViewGrid className="w-[35px] h-[35px] " />
-          <h3 className="text-gray-500 font-normal">{stat.title}  <span className={`text-xl font-semibold text-[30px] border-border-black ${stat.color}`}></span></h3>
-          <p className="text-xl font-semibold text-[30px]">{stat.value} <span className="text-customGray text-[14px] font-normal">{stat.decimal}</span> </p>
+          <h3 className="text-[#697598] font-[400] no-italic text-[14px] ">{stat.title}  <span className={`text-xl font-semibold text-[30px] border-border-black ${stat.color}`}></span></h3>
+          <p className=" font-normal no-italic font-haas text-[28px]">{stat.value} <span className="text-[#697598] text-[14px] font-normal">{stat.decimal}</span> </p>
           
           
     
@@ -36,3 +36,7 @@ export default InvoiceStats;
 // className={`p-4 bg-white shadow rounded-lg border-t-4 border-${stat.color} h-48 w-48 flex flex-col justify-center items-center`}
 // >
       {/* {`text-${stat.color} text-4xl`} */}
+
+
+
+
